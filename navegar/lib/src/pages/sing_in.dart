32f0f8823923ef27ca390c9a6ba55586da0a422b
unknown <sleonel1990@gmail.com>
2023-11-16 03:55:26 -0500

    navegacion de paginas

--- navegar/lib/src/pages/sing_in.dart
+++ navegar/lib/src/pages/sing_in.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class SingIn extends StatefulWidget {
   const SingIn({super.key});
 
@@ -11,7 +10,54 @@ class _SingInState extends State<SingIn> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Hola chicos"),
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: <Color>[
+                Color.fromARGB(255, 77, 199, 224),
+                Color.fromARGB(20, 184, 180, 154)
+              ],
+              begin: Alignment.topCenter,
+            ),
+          ),
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 200),
+            children: <Widget>[
+              // Aquí se ubica el URL de una imagen
+              SizedBox(
+                width:
+                    100.0, // Ajusta el ancho de la imagen según tus necesidades
+                height:
+                    100.0, // Ajusta la altura de la imagen según tus necesidades
+                child: Image.asset("images/lobo.jpg"),
+              ),
+              Text(
+                "LOGIN",
+                style: TextStyle(
+                  fontFamily: "PermanentMarker",
+                  fontSize: 30.0,
+                ),
+              ),
+              Divider(
+                height: 30.0,
+              ),
+              Text(
+                "Ejercicio N.-003",
+                style: TextStyle(
+                  fontFamily: "PermanentMarker",
+                  fontSize: 40.0,
+                ),
+              ),
+              Divider(
+                height: 30.0,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- navegar/lib/src/pages/sing_up.dart
+++ navegar/lib/src/pages/sing_up.dart
@@ -11,7 +11,54 @@ class _SingUpState extends State<SingUp> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Gracias por su comprención"),
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: <Color>[
+                Color.fromARGB(255, 77, 199, 224),
+                Color.fromARGB(20, 184, 180, 154)
+              ],
+              begin: Alignment.topCenter,
+            ),
+          ),
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 200),
+            children: <Widget>[
+              // Aquí se ubica el URL de una imagen
+              SizedBox(
+                width:
+                    100.0, // Ajusta el ancho de la imagen según tus necesidades
+                height:
+                    100.0, // Ajusta la altura de la imagen según tus necesidades
+                child: Image.asset("images/lobo.jpg"),
+              ),
+              Text(
+                "LOGIN",
+                style: TextStyle(
+                  fontFamily: "PermanentMarker",
+                  fontSize: 30.0,
+                ),
+              ),
+              Divider(
+                height: 30.0,
+              ),
+              Text(
+                "Ejercicio N.-003",
+                style: TextStyle(
+                  fontFamily: "PermanentMarker",
+                  fontSize: 40.0,
+                ),
+              ),
+              Divider(
+                height: 30.0,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
